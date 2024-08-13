cb7fb6f4bbd5f467b58585209d173b1b6aad2789
ssnider5 <ssnider5@outlook.com>
2024-08-13 14:46:10 -0500

    Update pb2.py

--- problems/pb2.py
+++ problems/pb2.py
@@ -1,4 +1,7 @@
 # declare and fill out function here
-
+def max_values2(numbers):
+  sortedList = sorted(numbers, reverse = True)
+  topTwo = (sortedList[0], sortedList[1])
+  return topTwo
 # test case
 print(max_values2([-5, -2, -1, -11])) # -> [1, 2]
